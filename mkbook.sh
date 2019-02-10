ec6a5c61e93baebbf2515e138aa3b006b22482ea
Jens-Uwe Mager <jum@anubis.han.de>
2019-02-10 13:52:46 -0800

    Section numbers do not look nice.

--- mkbook.sh
+++ mkbook.sh
@@ -1,2 +1,2 @@
-pandoc -t epub -o edocs.epub --toc-depth=2 -N edocs.yaml --resource-path=outdir outdir/*/*.md
+pandoc -t epub -o edocs.epub --toc-depth=2 edocs.yaml --resource-path=outdir outdir/*/*.md
 #ebook-convert edocs.epub edocs.mobi
