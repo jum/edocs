07035c83472eb274a178197c0e0e42cff3d2a842
Jens-Uwe Mager <jum@anubis.han.de>
2019-02-10 13:48:37 -0800

    Use pandoc options to better handle path problems, add section numbers.

--- mkbook.sh
+++ mkbook.sh
@@ -1,5 +1,2 @@
-(
-cd outdir
-pandoc -t epub -o ../edocs.epub --toc-depth=2 ../edocs.yaml */*.md
-)
+pandoc -t epub -o edocs.epub --toc-depth=2 -N edocs.yaml --resource-path=outdir outdir/*/*.md
 #ebook-convert edocs.epub edocs.mobi
